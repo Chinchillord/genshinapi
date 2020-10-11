044cafc044799bb148dbc11adc7ff3cf7906786a
Michael Rack <michael.rack@willowtreeapps.com>
2020-10-11 10:48:05 -0400

    add character entity

---
+++ src/main/kotlin/com/rack/genshinapi/entities/Character.kt
@@ -0,0 +1,9 @@
+package com.rack.genshinapi.entities
+
+data class Character (
+        val name: String,
+        val weaponType: String,
+        val vision: String,
+        val skill: String,
+        val burst: String
+)
